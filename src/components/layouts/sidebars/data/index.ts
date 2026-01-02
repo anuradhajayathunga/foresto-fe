e64195e2b1c11956a780e985d9e27096cdd3f3ac
Anuradha <176155511+anuradhajayathunga@users.noreply.github.com>
2026-01-03 04:20:09 +0530

    feat: changed side bar

--- src/components/layouts/sidebars/data/index.ts
+++ src/components/layouts/sidebars/data/index.ts
@@ -1,4 +1,3 @@
-
 import {
   Home,
   UtensilsCrossed,
@@ -14,11 +13,16 @@ import {
   LayoutDashboard,
   ClipboardList,
   CookingPot,
+  icons,
+  Truck,
+  Utensils,
+  Sparkle,
+  Sparkles,
 } from 'lucide-react';
 
 export const NAV_DATA = [
   {
-    label: 'Overview',
+    label: '  ',
     items: [
       {
         title: 'Dashboard',
@@ -26,66 +30,70 @@ export const NAV_DATA = [
         icon: LayoutDashboard,
         items: [],
       },
+    ],
+  },
+  {
+    label: 'MENU & KITCHEN',
+    items: [
       {
-        title: 'Sales (POS)',
-        url: '/sales',
-        icon: Receipt,
+        title: 'Menu Items',
+        url: '/menu',
+        icon: Utensils,
+        items: [],
+      },
+      {
+        title: 'Recipes',
+        url: '/recipes',
+        icon: CookingPot,
         items: [],
       },
     ],
   },
   {
-    label: 'Management',
+    label: 'INVENTORY & SUPPLY',
     items: [
-      {
-        title: 'Menu Items',
-        url: '/menu',
-        icon: UtensilsCrossed,
-        items: [],
-      },
       {
         title: 'Inventory',
         url: '/inventory',
         icon: Package,
         items: [],
       },
-            {
+      {
         title: 'Purchases',
         url: '/purchases',
         icon: ClipboardList,
         items: [],
       },
-      {
-        title: 'Kitchen',
-        url: '/recipes',
-        icon: CookingPot,
-        items: [],
-      },
-    ]
+      { title: 'Suppliers', url: '/suppliers', icon: Truck, items: [] },
+    ],
   },
   {
     label: 'Business',
     items: [
       {
-        title: 'Staff & Users',
-        icon: UserCog,
-        items: [
-          { title: 'Employees', url: '/staff', items: [] },
-          { title: 'Suppliers', url: '/suppliers', items: [] },
-        ],
-      },
-      {
-        title: 'Analytics',
-        url: '/analytics',
-        icon: BarChart3,
+        title: 'Sales (POS)',
+        url: '/sales',
+        icon: Receipt,
         items: [],
       },
       {
-        title: 'Settings',
-        url: '/settings',
-        icon: Settings,
+        title: 'AI Forecasting',
+        url: '/forecating',
+        icon: Sparkles,
         items: [],
       },
+      // {
+      //   title: 'Staff & Users',
+      //   icon: UserCog,
+      //   items: [{ title: 'Employees', url: '/staff', items: [] }],
+      // },
+
+      // {
+      //   title: 'Settings',
+      //   url: '/settings',
+      //   icon: Settings,
+      //   items: [],
+      // },
     ],
   },
 ];
